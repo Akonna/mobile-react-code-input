844e84933405f6fa43a9e7b7142697f98fef62bb
Akonna <kangnajiayou@foxmail.com>
2017-09-27 17:10:35 +0800

    no message

--- examples/verifyCode.js
+++ examples/verifyCode.js
@@ -6,10 +6,7 @@ const VerifyCode = require('react-input-code-pretty');
 const Test = React.createClass({
  useCodeFun(valuestr){
     console.log(valuestr);
-
-},
-
-
+ },
   render() {
     return (<div style={{margin: 0}}>
       <VerifyCode isShow={true} useCodeFun={this.useCodeFun} btnText="立即使用"/>
--- src/VerifyCode.jsx
+++ src/VerifyCode.jsx
@@ -16,27 +16,63 @@ const VerifyCode = React.createClass({
   },
   getInitialState() {
     return {
-     flag_two: false,
+      flag_two: false,
       flag_three: false,
       code: "N",
     };
   },
  
    one(event) {
-    if (event.target.value) {
+    if (event.target.value && !this.refs.two.value) {
       this.refs.two.focus();
       this.setState({
         flag_two: true,
       })
     }
+    if(!event.target.value){
+      this.setState({
+        code: 'N',
+        flag_two: false,
+      });
+    }
+     if(!event.target.value && !this.refs.two.value){
+       this.setState({
+         flag_two: false,
+       });
+     }else{
+       this.setState({
+         flag_two: true,
+       });
+     }
+    if(event.target.value && this.refs.two.value && this.refs.three.value){
+      this.setState({
+        code: 'Y',
+      });
+    }
   },
 
   two(event){
-    if (event.target.value) {
+    if (event.target.value && !this.refs.three.value) {
       this.refs.three.focus();
       this.setState({
         flag_three: true,
       })
+    }
+    if(!event.target.value){
+      this.setState({
+        code: 'N',
+        flag_three: false,
+      });
+    }
+    if(this.refs.three.value){
+      this.setState({
+        flag_three: true,
+      });
+    }
+    if(event.target.value && this.refs.two.value && this.refs.three.value){
+      this.setState({
+        code: 'Y',
+      });
     }
   },
 
@@ -47,12 +83,36 @@ const VerifyCode = React.createClass({
         code: 'Y',
       });
     }
+    if(!event.target.value){
+      this.setState({
+        code: 'N',
+      });
+    }
 
+    if(!this.refs.two.value){
+      this.setState({
+        flag_three: false,
+      });
+    }
+    if(!this.refs.one.value){
+      this.setState({
+        flag_two: false,
+      });
+    }
+    if(event.target.value && this.refs.two.value && this.refs.three.value){
+      this.setState({
+        code: 'Y',
+      });
+    }
   },
 
   commonfun(){
     if (!this.refs.one.value) {
       this.refs.one.focus();
+    }else{
+      if(!this.refs.two.value){
+        this.refs.two.focus();
+      }
     }
   },
 
@@ -61,11 +121,18 @@ const VerifyCode = React.createClass({
     this.props.useCodeFun(input_val);
   },
   render() {
-   
+    if(!this.props.isShow){
+      if(this.refs.one!=undefined && this.refs.two!=undefined && this.refs.three!=undefined){
+        if(this.refs.one.value &&this.refs.two.value && this.refs.three.value ){
+          let input_val =this.refs.one.value + this.refs.two.value + this.refs.three.value;
+          this.props.useCodeFun(input_val);
+        }
+      }
+    }
     return (
-      <div>
+      <div className="pretty">
         <div className="box">
-          <input type="tel" className="inputbox select" maxLength="1" ref="one" onChange={this.one}/>
+          <input type="tel" className="inputbox select" maxLength="1" ref="one" onChange={this.one} onFocus={this.commonfun}/>
           <input type="tel" className={this.state.flag_two ? "inputbox select" : "inputbox"} maxLength="1" ref="two"
                  onChange={this.two} onFocus={this.commonfun}/>
           <input type="tel" className={this.state.flag_three ? "inputbox select" : "inputbox"} maxLength="1" ref="three"

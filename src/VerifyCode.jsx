a293229f3a11826a63ec415b053939db6e6797fd
Akonna <kangnajiayou@foxmail.com>
2017-10-27 15:03:58 +0800

    no message

--- src/VerifyCode.jsx
+++ src/VerifyCode.jsx
@@ -4,142 +4,134 @@ function noop() {
   
 }
 const VerifyCode = React.createClass({
-  propTypes: {
-    useCodeFun: React.PropTypes.func,
-  },
-  getDefaultProps() {
-    return {
-      isShow: false,
-      useCodeFun:noop,
-      btnText:'',
-      empty:false,
-    };
-  },
-
-   getInitialState() {
-    return{
-      active:0,
-      twoFlag: false,
-      threeFlag: false,
-      code:'N',
-    }
-   },
-   componentWillReceiveProps(nextProps) {
-     if(!nextProps.empty){
-        this.setState({
-          active:0,
-          twoFlag: false,
-          threeFlag: false,
-        })
-     }
-   },
-    componentDidMount() {
-      
-      var This= this;
-      var inputBtn = document.querySelectorAll('input');
-        
-        for (var i = 0; i < inputBtn.length; i++) {
-
-          inputBtn[i].addEventListener('focus', function (e) {
-            e.preventDefault();
-            this.addEventListener('keyup', This.listenKeyUp, false);
-          }, false);
-           inputBtn[i].addEventListener('blur', function (e) {
-               e.preventDefault();
-            this.removeEventListener('keyup', This.listenKeyUp, false);
-          }, false);
-         }
+    propTypes: {
+        useCodeFun: React.PropTypes.func,
     },
-    listenKeyUp(e){
-        e.preventDefault();
-        var inputBtn = document.querySelectorAll('input');
-          var re =  /^[0-9]+[0-9]*]*$/; //判断字符串是否为数字
+    getDefaultProps() {
+        return {
+            isShow: false,
+            useCodeFun:noop,
+            btnText:'',
+            empty:false,
+        };
+    },
+    getInitialState() {
+        return {
+            code: "N",
+            active:0,
+            twoFlag: false,
+            threeFlag: false,
+            oneValue:"",
+            twoValue:"",
+            threeValue:"",
+        };
+    },
 
-         if (!re.test(event.target.value)) {
-           event.target.value="";
-         }
-        if (!isNaN(event.target.value) && event.target.value.length != 0) {
-            if(this.state.active == "0" && !isNaN(event.target.value) && event.target.value.length != 0){
-                  this.setState({
-                    twoFlag:true,
-                  })
-               }else if(this.state.active == "1" && !isNaN(event.target.value) && event.target.value.length != 0){
-                 this.setState({
-                    threeFlag:true,
-                  })
-               }
-            if (this.state.active < 2) {
-              this.setState({
-                active:this.state.active +1,
-              });
-            }
-            inputBtn[this.state.active].focus();
-          
-        } else if (event.target.value.length == 0) {
-           if(this.state.active == '2'){
+    componentWillReceiveProps(nextProps) {
+        if(!nextProps.empty){
+            this.setState({
+                code: "N",
+                active:0,
+                twoFlag: false,
+                threeFlag: false,
+                oneValue:"",
+                twoValue:"",
+                threeValue:"",
+            })
+        }
+    },
+
+    useCodeFun(){
+        this.props.useCodeFun(this.refs.input.value);
+    },
+
+    changeInput(event){
+        var re =  /^[0-9]+[0-9]*]*$/; //判断字符串是否为数字
+        if (!re.test(event.target.value)) {
+            event.target.value="";
+        }
+
+        if(this.state.active == 0){
+            if(!isNaN(event.target.value) && event.target.value.length == '1'){
                 this.setState({
-                  threeFlag:false,
+                    twoFlag:true,
+                    oneValue:event.target.value,
                 })
             }
-            if(this.state.active == '1'){
+            if(!isNaN(event.target.value) && event.target.value.length == '2'){
                 this.setState({
-                  twoFlag:false,
-                }) 
+                    threeFlag:true,
+                    twoValue:event.target.value.charAt(event.target.value.length-1),
+                })
             }
-            if (this.state.active > 0) {
-               this.setState({
-                active:this.state.active -1,
-              });
+            if(!isNaN(event.target.value) && event.target.value.length == '3'){
+                this.refs.input.blur();
+                this.setState({
+                    code:'Y',
+                    threeValue:event.target.value.charAt(event.target.value.length-1),
+                    active:1,
+                })
             }
-            inputBtn[this.state.active].focus();
-            
+        }else{
+            console.log(!isNaN(event.target.value));
+            if(!isNaN(event.target.value) && event.target.value.length == '2'){
+                this.setState({
+                    code:'N',
+                    threeValue:"",
+                    threeFlag:false,
+                })
+            }
+            if(!isNaN(event.target.value) && event.target.value.length == '1'){
+                this.setState({
+                    twoValue:"",
+                    twoFlag:false,
+                })
+            }
+            if(!isNaN(event.target.value) && event.target.value.length == '0'){
+                this.setState({
+                    oneValue:"",
+                    active:0,
+                })
+            }
         }
 
-         if(this.refs.one.value && this.refs.two.value && this.refs.three.value){
-          this.setState({
-            code: 'Y',
-          });
-        }else{
-          this.setState({
-            code: 'N',
-          });
-        }
-                 
+
     },
 
-   common(){
-    var inputBtn = document.querySelectorAll('input');
-       inputBtn[this.state.active].focus();
-   },
+    clickInput(event){
+        this.refs.input.focus();
+    },
 
-  useCodeFun(){
-    let input_val = this.refs.one.value + this.refs.two.value + this.refs.three.value;
-    this.props.useCodeFun(input_val);
-  },
-  render(){
-   
-    if(!this.props.isShow){
-      if(this.refs.one!=undefined && this.refs.two!=undefined && this.refs.three!=undefined){
-          let input_val =this.refs.one.value + this.refs.two.value + this.refs.three.value;
-          this.props.useCodeFun(input_val);
-      }
-    }
-
-        return(
-           <div className="box">
-             <input   type="text" pattern="\d*"  maxLength="1"  onClick={this.common} className={"inputBtn select"} ref="one"/>
-             <input  type="text" pattern="\d*"  maxLength="1" onClick={this.common} className={this.state.twoFlag ? "inputBtn select" : "inputBtn"} ref="two"/>
-             <input   type="text" pattern="\d*"  maxLength="1" onClick={this.common} className={this.state.threeFlag ? "inputBtn select" : "inputBtn"} ref="three"/>
-
-             {
-          this.props.isShow ? (this.state.code == 'Y' ?
-            <div className="ver-btn click_btn" onClick={this.useCodeFun}>{this.props.btnText}</div> :
-            <div className="ver-btn">{this.props.btnText}</div>) : ''
+    render() {
+        if(!this.props.isShow){
+            if(this.refs.input.value !==undefined){
+                this.props.useCodeFun(this.refs.input.value);
+            }
         }
 
-          </div>
+        return (
+            <div className="pretty">
+                <input  type="text" unselectable="on"  pattern="\d*" className="inputTruebox"  maxLength="3" ref="input"  onChange={this.changeInput}/>
+                <div className="box">
+                    <div className="inputbox select" onClick={this.clickInput}>
+                        {this.state.oneValue}
+                    </div>
+                    <div className={this.state.twoFlag ? "inputbox select": "inputbox"} onClick={this.clickInput}>
+                        {this.state.twoValue}
+                    </div>
+                    <div className={this.state.threeFlag ? "inputbox select": "inputbox"} onClick={this.clickInput}>
+                        {this.state.threeValue}
+                    </div>
+                </div>
+
+                {
+                    this.props.isShow ? (this.state.code == 'Y' ?
+                        <div className="ver-btn click_btn" onClick={this.useCodeFun}>{this.props.btnText}</div> :
+                        <div className="ver-btn">{this.props.btnText}</div>) : ''
+                }
+            </div>
         )
-    }
+    },
 });
 
 

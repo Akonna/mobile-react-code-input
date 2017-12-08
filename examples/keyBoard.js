da75b6437b5b4d4402b8985c3d4af0cdd6bb5938
Akonna <kangnajiayou@foxmail.com>
2017-12-08 22:43:38 +0800

    Updates

--- examples/keyBoard.js
+++ examples/keyBoard.js
@@ -1,14 +1,14 @@
-webpackJsonp([0],{
+webpackJsonp([1],{
 
 /***/ 0:
 /***/ (function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(1);
+	module.exports = __webpack_require__(161);
 
 
 /***/ }),
 
-/***/ 1:
+/***/ 161:
 /***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -16,7 +16,7 @@ webpackJsonp([0],{
 	__webpack_require__(2);
 	var React = __webpack_require__(3);
 	var ReactDOM = __webpack_require__(160);
-	var KeyBoard = __webpack_require__(161);
+	var KeyBoard = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"react-input-code-pretty/src/KeyBoard\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	
 	var Test = React.createClass({
 	  displayName: 'Test',
@@ -34,159 +34,6 @@ webpackJsonp([0],{
 	});
 	
 	ReactDOM.render(React.createElement(Test, null), document.getElementById('__react-content'));
-
-/***/ }),
-
-/***/ 161:
-/***/ (function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	var React = __webpack_require__(3);
-	function noop() {}
-	
-	var KeyBoard = React.createClass({
-	  displayName: 'KeyBoard',
-	
-	  propTypes: {
-	    useCodeFun: React.PropTypes.func
-	  },
-	  getDefaultProps: function getDefaultProps() {
-	    return {
-	      isShow: false,
-	      useCodeFun: noop,
-	      btnText: '',
-	      empty: false
-	    };
-	  },
-	
-	  getInitialState: function getInitialState() {
-	    return {
-	      active: 0,
-	      twoFlag: false,
-	      threeFlag: false,
-	      code: 'N'
-	    };
-	  },
-	  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-	    if (!nextProps.empty) {
-	      this.setState({
-	        active: 0,
-	        twoFlag: false,
-	        threeFlag: false
-	      });
-	    }
-	  },
-	  componentDidMount: function componentDidMount() {
-	
-	    var This = this;
-	    var inputBtn = document.querySelectorAll('input');
-	
-	    for (var i = 0; i < inputBtn.length; i++) {
-	
-	      inputBtn[i].addEventListener('focus', function (e) {
-	        e.preventDefault();
-	        this.addEventListener('keyup', This.listenKeyUp, false);
-	      }, false);
-	      inputBtn[i].addEventListener('blur', function (e) {
-	        e.preventDefault();
-	        this.removeEventListener('keyup', This.listenKeyUp, false);
-	      }, false);
-	    }
-	  },
-	  listenKeyUp: function listenKeyUp(e) {
-	    e.preventDefault();
-	    var inputBtn = document.querySelectorAll('input');
-	    var re = /^[0-9]+[0-9]*]*$/; //判断字符串是否为数字
-	
-	    if (!re.test(event.target.value)) {
-	      event.target.value = "";
-	    }
-	    if (!isNaN(event.target.value) && event.target.value.length != 0) {
-	      if (this.state.active == "0" && !isNaN(event.target.value) && event.target.value.length != 0) {
-	        this.setState({
-	          twoFlag: true
-	        });
-	      } else if (this.state.active == "1" && !isNaN(event.target.value) && event.target.value.length != 0) {
-	        this.setState({
-	          threeFlag: true
-	        });
-	      }
-	      if (this.state.active < 2) {
-	        this.setState({
-	          active: this.state.active + 1
-	        });
-	      }
-	      inputBtn[this.state.active].focus();
-	    } else if (event.target.value.length == 0) {
-	      if (this.state.active == '2') {
-	        this.setState({
-	          threeFlag: false
-	        });
-	      }
-	      if (this.state.active == '1') {
-	        this.setState({
-	          twoFlag: false
-	        });
-	      }
-	      if (this.state.active > 0) {
-	        this.setState({
-	          active: this.state.active - 1
-	        });
-	      }
-	      inputBtn[this.state.active].focus();
-	    }
-	
-	    if (this.refs.one.value && this.refs.two.value && this.refs.three.value) {
-	      this.setState({
-	        code: 'Y'
-	      });
-	    } else {
-	      this.setState({
-	        code: 'N'
-	      });
-	    }
-	  },
-	
-	  common: function common() {
-	    var inputBtn = document.querySelectorAll('input');
-	    inputBtn[this.state.active].focus();
-	  },
-	
-	  useCodeFun: function useCodeFun() {
-	    var input_val = this.refs.one.value + this.refs.two.value + this.refs.three.value;
-	    this.props.useCodeFun(input_val);
-	  },
-	  render: function render() {
-	
-	    if (!this.props.isShow) {
-	      if (this.refs.one != undefined && this.refs.two != undefined && this.refs.three != undefined) {
-	        var input_val = this.refs.one.value + this.refs.two.value + this.refs.three.value;
-	        this.props.useCodeFun(input_val);
-	      }
-	    }
-	
-	    return React.createElement(
-	      'div',
-	      { className: 'box' },
-	      React.createElement('input', { type: 'text', pattern: '\\d*', maxLength: '1', onClick: this.common, className: "inputBtn select", ref: 'one' }),
-	      React.createElement('input', { type: 'text', pattern: '\\d*', maxLength: '1', onClick: this.common, className: this.state.twoFlag ? "inputBtn select" : "inputBtn", ref: 'two' }),
-	      React.createElement('input', { type: 'text', pattern: '\\d*', maxLength: '1', onClick: this.common, className: this.state.threeFlag ? "inputBtn select" : "inputBtn", ref: 'three' }),
-	      this.props.isShow ? this.state.code == 'Y' ? React.createElement(
-	        'div',
-	        { className: 'ver-btn click_btn', onClick: this.useCodeFun },
-	        this.props.btnText
-	      ) : React.createElement(
-	        'div',
-	        { className: 'ver-btn' },
-	        this.props.btnText
-	      ) : ''
-	    );
-	  }
-	
-	});
-	
-	module.exports = KeyBoard;
 
 /***/ })
 
--- examples/verifyCode.js
+++ examples/verifyCode.js
@@ -1,4 +1,4 @@
-webpackJsonp([1],{
+webpackJsonp([2],{
 
 /***/ 0:
 /***/ (function(module, exports, __webpack_require__) {
@@ -16,7 +16,7 @@ webpackJsonp([1],{
 	__webpack_require__(2);
 	var React = __webpack_require__(3);
 	var ReactDOM = __webpack_require__(160);
-	var VerifyCode = __webpack_require__(163);
+	var VerifyCode = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"react-input-code-pretty/src/VerifyCode\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	
 	var Test = React.createClass({
 	  displayName: 'Test',
@@ -34,160 +34,6 @@ webpackJsonp([1],{
 	});
 	
 	ReactDOM.render(React.createElement(Test, null), document.getElementById('__react-content'));
-
-/***/ }),
-
-/***/ 163:
-/***/ (function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	var React = __webpack_require__(3);
-	
-	function noop() {}
-	var VerifyCode = React.createClass({
-	    displayName: 'VerifyCode',
-	
-	    propTypes: {
-	        useCodeFun: React.PropTypes.func
-	    },
-	    getDefaultProps: function getDefaultProps() {
-	        return {
-	            isShow: false,
-	            useCodeFun: noop,
-	            btnText: '',
-	            empty: false
-	        };
-	    },
-	    getInitialState: function getInitialState() {
-	        return {
-	            code: "N",
-	            active: 0,
-	            twoFlag: false,
-	            threeFlag: false,
-	            oneValue: "",
-	            twoValue: "",
-	            threeValue: ""
-	        };
-	    },
-	
-	    componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
-	        if (!nextProps.empty) {
-	            this.setState({
-	                code: "N",
-	                active: 0,
-	                twoFlag: false,
-	                threeFlag: false,
-	                oneValue: "",
-	                twoValue: "",
-	                threeValue: ""
-	            });
-	        }
-	    },
-	
-	    useCodeFun: function useCodeFun() {
-	        this.props.useCodeFun(this.refs.input.value);
-	    },
-	
-	    changeInput: function changeInput(event) {
-	        var re = /^[0-9]+[0-9]*]*$/; //判断字符串是否为数字
-	        if (!re.test(event.target.value)) {
-	            event.target.value = "";
-	        }
-	
-	        if (this.state.active == 0) {
-	            if (!isNaN(event.target.value) && event.target.value.length == '1') {
-	                this.setState({
-	                    twoFlag: true,
-	                    oneValue: event.target.value
-	                });
-	            }
-	            if (!isNaN(event.target.value) && event.target.value.length == '2') {
-	                this.setState({
-	                    threeFlag: true,
-	                    twoValue: event.target.value.charAt(event.target.value.length - 1)
-	                });
-	            }
-	            if (!isNaN(event.target.value) && event.target.value.length == '3') {
-	                this.refs.input.blur();
-	                this.setState({
-	                    code: 'Y',
-	                    threeValue: event.target.value.charAt(event.target.value.length - 1),
-	                    active: 1
-	                });
-	            }
-	        } else {
-	            console.log(!isNaN(event.target.value));
-	            if (!isNaN(event.target.value) && event.target.value.length == '2') {
-	                this.setState({
-	                    code: 'N',
-	                    threeValue: "",
-	                    threeFlag: false
-	                });
-	            }
-	            if (!isNaN(event.target.value) && event.target.value.length == '1') {
-	                this.setState({
-	                    twoValue: "",
-	                    twoFlag: false
-	                });
-	            }
-	            if (!isNaN(event.target.value) && event.target.value.length == '0') {
-	                this.setState({
-	                    oneValue: "",
-	                    active: 0
-	                });
-	            }
-	        }
-	    },
-	
-	    clickInput: function clickInput(event) {
-	        this.refs.input.focus();
-	    },
-	
-	    render: function render() {
-	        if (!this.props.isShow) {
-	            if (this.refs.input.value !== undefined) {
-	                this.props.useCodeFun(this.refs.input.value);
-	            }
-	        }
-	
-	        return React.createElement(
-	            'div',
-	            { className: 'pretty' },
-	            React.createElement('input', { type: 'text', unselectable: 'on', pattern: '\\d*', className: 'inputTruebox', maxLength: '3', ref: 'input', onChange: this.changeInput }),
-	            React.createElement(
-	                'div',
-	                { className: 'box' },
-	                React.createElement(
-	                    'div',
-	                    { className: 'inputbox select', onClick: this.clickInput },
-	                    this.state.oneValue
-	                ),
-	                React.createElement(
-	                    'div',
-	                    { className: this.state.twoFlag ? "inputbox select" : "inputbox", onClick: this.clickInput },
-	                    this.state.twoValue
-	                ),
-	                React.createElement(
-	                    'div',
-	                    { className: this.state.threeFlag ? "inputbox select" : "inputbox", onClick: this.clickInput },
-	                    this.state.threeValue
-	                )
-	            ),
-	            this.props.isShow ? this.state.code == 'Y' ? React.createElement(
-	                'div',
-	                { className: 'ver-btn click_btn', onClick: this.useCodeFun },
-	                this.props.btnText
-	            ) : React.createElement(
-	                'div',
-	                { className: 'ver-btn' },
-	                this.props.btnText
-	            ) : ''
-	        );
-	    }
-	});
-	
-	module.exports = VerifyCode;
 
 /***/ })
 
